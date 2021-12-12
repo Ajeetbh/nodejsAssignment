2e5e0bcb1e2b43f8605f1b61d91779c12ec81d13
Ajeet <ajeetbharti279@gmail.com>
2021-12-12 23:16:25 +0530

    assignment problem no 2

---
+++ node.js
@@ -0,0 +1,18 @@
+const vegetable={
+    'result':{
+        'rootVegetables':[
+            'turnips',
+            'ginger',
+            'beets'
+        ],
+        'leafyVegetables':[
+            'broccoli',
+            'spinach',
+            'cabbage'
+       ]
+    } 
+};
+//res.end(JSON.stringify(vegetable))
+module.exports=vegetable
+
+//console.log(vegetable)
--- server.js
+++ server.js
@@ -1,1 +1,36 @@
-console.log('Hello')
+//1:
+// const http=require('http');
+// const server=http.createServer((req,res)=>{
+//     res.writeHead(200,{'Content-Type':'aplication/json'});
+//     var vegetable={
+//         'result':{
+//             'rootVegetables':[
+//                 'turnips',
+//                 'ginger',
+//                 'beets'
+//             ],
+//             'leafyVegetables':[
+//                 'broccoli',
+//                 'spinach',
+//                 'cabbage'
+//             ]
+//         }
+//     };
+//     res.end(JSON.stringify(vegetable))
+// })
+// server.listen(8000,()=>{
+//     console.log('app is running')
+// })
+
+
+const http = require('http');
+const fs = require('fs');
+const node=require('./node.js')
+node.use('/vegetable',node)
+http.createServer(function (req, res) {
+  fs.readFile('node.js', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/js'});
+    res.write(data);
+    return res.end();
+  });
+}).listen(8080);
